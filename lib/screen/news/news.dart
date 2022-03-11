2a3edf70e3b7431491bba76b000535c2d89a78f7
simata98 <wlsrnr905@naver.com>
2022-03-11 17:57:04 +0900

    news 기능 추가, camera 기능 추가중

---
+++ lib/screen/news/news.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './detailspage.dart';
+
+class News extends StatefulWidget {
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  List data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetch_data_from_api();
+  }
+
+  Future<String> fetch_data_from_api() async {
+    var jsondata = await http.get(
+        Uri.parse("http://newsapi.org/v2/everything?q=environment&apiKey=7368ad3de2d543aeb2ce539dfbcb6f1d"),);
+    var fetchdata = jsonDecode(jsondata.body);
+    setState(() {
+      data = fetchdata["articles"];
+    });
+    return "Success";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "News App",
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("News App"),
+            ),
+            body: Padding(
+              padding: EdgeInsets.only(top: 30.0),
+              child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailsPage(
+                                    author: data[index]["author"],
+                                    title: data[index]["title"],
+                                    description: data[index]["description"],
+                                    urlToImage: data[index]["urlToImage"],
+                                    publishedAt: data[index]["publishedAt"],
+                                  )));
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(35.0),
+                              topRight: Radius.circular(35.0),
+                            ),
+                            child: Image.network(
+                              data[index]["urlToImage"],
+                              fit: BoxFit.cover,
+                              height: 400.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0.0, 350.0, 0.0, 0.0),
+                          child: Container(
+                            height: 200.0,
+                            width: 400.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(35.0),
+                              elevation: 10.0,
+                              child: Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        20.0, 20.0, 10.0, 20.0),
+                                    child: Text(
+                                      data[index]["title"],
+                                      style: TextStyle(
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: data == null ? 0 : data.length,
+                autoplay: true,
+                viewportFraction: 0.8,
+                scale: 0.9,
+              ),
+            )));
+  }
+}
