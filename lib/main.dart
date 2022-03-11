55460372f8d0d2cee6e60dd8c00f5f99bab8b85b
choochoo <cnckddn0146@gmail.com>
2022-03-12 04:34:16 +0900

    Commit Example Map

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,19 @@ import 'package:gdsc_solution/screen/login/sign_in.dart';
 import 'package:get/route_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async{
+import 'package:gdsc_solution/screen/main/main.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({ Key? key }) : super(key: key);
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SignIn()
-    );
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: mapMain());
   }
 }
