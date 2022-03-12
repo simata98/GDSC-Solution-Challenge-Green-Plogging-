72b025f009b122905b743be245052fda5eac3dff
choochoo <cnckddn0146@gmail.com>
2022-03-13 00:33:33 +0900

    라인트래킹 구현

--- lib/screen/main/main.dart
+++ lib/screen/main/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:location/location.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-//테스트
-final LatLngBounds sydneyBounds = LatLngBounds(
-  southwest: const LatLng(-34.022631, 150.620685),
-  northeast: const LatLng(-33.571835, 151.325952),
-);
+import 'package:gdsc_solution/model/geo_entry.dart';
 
 class mapMain extends StatefulWidget {
   mapMain({Key? key}) : super(key: key);
@@ -19,38 +21,215 @@ class _mapMainState extends State<mapMain> {
   _mapMainState();
 
   Completer<GoogleMapController> _controller = Completer();
+  Map<PolylineId, Polyline> polylines = <PolylineId, Polyline>{};
+  PolylineId? selectedPolyline;
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  final Set<Polyline> polyline = {};
+  Location _location = Location();
+  GoogleMapController? _mapController;
+  LatLng _center = const LatLng(0, 0);
+  List<LatLng> route = [];
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  double _dist = 0;
+  String _displayTime = "";
+  int _time = 0;
+  int _lastTime = 0;
+  double _speed = 0;
+  double _avgSpeed = 0;
+  int _speedCounter = 0;
+
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+  void initState() {
+    super.initState();
+    _stopWatchTimer.onExecute.add(StopWatchExecute.start);
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose(); // Need to call dispose function.
+  }
+
+  void _onPolylineTapped(PolylineId polylineId) {
+    setState(() {
+      selectedPolyline = polylineId;
+    });
+  }
+
+  void _remove(PolylineId polylineId) {
+    setState(() {
+      if (polylines.containsKey(polylineId)) {
+        polylines.remove(polylineId);
+      }
+      selectedPolyline = null;
+    });
+  }
+
+  void _add() {
+    final PolylineId polylineId = PolylineId('polyline');
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.orange,
+      width: 5,
+      points: _createPoints(),
+      onTap: () {
+        _onPolylineTapped(polylineId);
+      },
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  List<LatLng> _createPoints() {
+    final List<LatLng> points = <LatLng>[];
+
+    points.add(_createLatLng(51.4816, -3.1791));
+    points.add(_createLatLng(53.0430, -2.9925));
+    points.add(_createLatLng(53.1396, -4.2739));
+    points.add(_createLatLng(52.4153, -4.0829));
+    return points;
+  }
+
+  LatLng _createLatLng(double lat, double lng) {
+    return LatLng(lat, lng);
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    double appendDist;
+
+    _location.onLocationChanged.listen((event) {
+      LatLng loc = LatLng(event.latitude!, event.longitude!);
+      _mapController!.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(target: loc, zoom: 15)));
+
+      if (route.length > 0) {
+        appendDist = Geolocator.distanceBetween(route.last.latitude,
+            route.last.longitude, loc.latitude, loc.longitude);
+        _dist = _dist + appendDist;
+        int timeDuration = (_time - _lastTime);
+
+        if (_lastTime != null && timeDuration != 0) {
+          _speed = (appendDist / (timeDuration / 100)) * 3.6;
+          if (_speed != 0) {
+            _avgSpeed = _avgSpeed + _speed;
+            _speedCounter++;
+          }
+        }
+      }
+      _lastTime = _time;
+      route.add(loc);
+
+      polyline.add(Polyline(
+          polylineId: PolylineId(event.toString()),
+          visible: true,
+          points: route,
+          width: 5,
+          startCap: Cap.roundCap,
+          endCap: Cap.roundCap,
+          color: Colors.deepOrange));
+
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Check IOS Platform
+    final bool isIOS = !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
+
+    return Scaffold(
+        body: Stack(children: [
+      Container(
+          child: GoogleMap(
+        polylines: polyline,
+        zoomControlsEnabled: false,
+        onMapCreated: _onMapCreated,
+        myLocationEnabled: true,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 11),
+      )),
+      Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: double.infinity,
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 40),
+            height: 140,
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text("SPEED (KM/H)",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 10, fontWeight: FontWeight.w300)),
+                        Text(_speed.toStringAsFixed(2),
+                            style: GoogleFonts.montserrat(
+                                fontSize: 30, fontWeight: FontWeight.w300))
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("TIME",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 10, fontWeight: FontWeight.w300)),
+                        StreamBuilder<int>(
+                          stream: _stopWatchTimer.rawTime,
+                          initialData: 0,
+                          builder: (context, snap) {
+                            _time = snap.data!;
+                            _displayTime =
+                                StopWatchTimer.getDisplayTimeHours(_time) +
+                                    ":" +
+                                    StopWatchTimer.getDisplayTimeMinute(_time) +
+                                    ":" +
+                                    StopWatchTimer.getDisplayTimeSecond(_time);
+                            return Text(_displayTime,
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 30, fontWeight: FontWeight.w300));
+                          },
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("DISTANCE (KM)",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 10, fontWeight: FontWeight.w300)),
+                        Text((_dist / 1000).toStringAsFixed(2),
+                            style: GoogleFonts.montserrat(
+                                fontSize: 30, fontWeight: FontWeight.w300))
+                      ],
+                    )
+                  ],
+                ),
+                Divider(),
+                IconButton(
+                  icon: Icon(
+                    Icons.stop_circle_outlined,
+                    size: 50,
+                    color: Colors.redAccent,
+                  ),
+                  padding: EdgeInsets.all(0),
+                  onPressed: () async {
+                    Entry en = Entry(
+                        date: DateFormat.yMMMMd('en_US').format(DateTime.now()),
+                        duration: _displayTime,
+                        speed:
+                            _speedCounter == 0 ? 0 : _avgSpeed / _speedCounter,
+                        distance: _dist);
+                    Navigator.pop(context, en);
+                  },
+                )
+              ],
+            ),
+          ))
+    ]));
   }
 }
