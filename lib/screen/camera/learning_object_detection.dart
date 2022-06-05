7ad89a5b3d644c0805ef871723819dca25e9f853
simata98 <wlsrnr905@naver.com>
2022-06-05 23:21:39 +0900

    카메라 테스트(맥북으로 옮기기)

--- lib/screen/camera/learning_object_detection.dart
+++
@@ -1,5 +0,0 @@
-library learning_object_detection;
-
-export 'src/detector.dart';
-export 'src/overlay.dart';
-export 'src/object.dart';
--- lib/screen/camera/main.dart
+++ lib/screen/camera/main.dart
@@ -1,9 +1,20 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:learning_input_image/learning_input_image.dart';
-import 'package:learning_object_detection/learning_object_detection.dart';
-import 'package:provider/provider.dart';
 
-void main() {
+import 'nlp_detector_views/entity_extraction_view.dart';
+import 'nlp_detector_views/language_identifier_view.dart';
+import 'nlp_detector_views/language_translator_view.dart';
+import 'nlp_detector_views/smart_reply_view.dart';
+import 'vision_detector_views/object_detector_view.dart';
+
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
   runApp(MyApp());
 }
 
@@ -12,124 +23,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryTextTheme: TextTheme(headline6: TextStyle(color: Colors.white)),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google ML Kit Demo App'),
+        centerTitle: true,
+        elevation: 0,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => ObjectDetectionState(),
-        child: ObjectDetectionPage(),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  ExpansionTile(
+                    title: const Text('Vision APIs'),
+                    children: [
+                      CustomCard('Object Detection', ObjectDetectorView()),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ExpansionTile(
+                    title: const Text('Natural Language APIs'),
+                    children: [
+                      CustomCard('Language ID', LanguageIdentifierView()),
+                      CustomCard(
+                          'On-device Translation', LanguageTranslatorView()),
+                      CustomCard('Smart Reply', SmartReplyView()),
+                      CustomCard('Entity Extraction', EntityExtractionView()),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class ObjectDetectionPage extends StatefulWidget {
-  @override
-  _ObjectDetectionPageState createState() => _ObjectDetectionPageState();
-}
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
 
-class _ObjectDetectionPageState extends State<ObjectDetectionPage> {
-  ObjectDetectionState get state => Provider.of(context, listen: false);
-
-  ObjectDetector _detector = ObjectDetector(
-    isStream: false,
-    enableClassification: true,
-    enableMultipleObjects: true,
-  );
-
-  @override
-  void dispose() {
-    _detector.dispose();
-    super.dispose();
-  }
-
-  Future<void> _detectObjects(InputImage image) async {
-    if (state.isNotProcessing) {
-      state.startProcessing();
-      state.image = image;
-      state.data = await _detector.detect(image);
-      state.stopProcessing();
-    }
-  }
+  const CustomCard(this._label, this._viewPage, {this.featureCompleted = true});
 
   @override
   Widget build(BuildContext context) {
-    return InputCameraView(
-      cameraDefault: InputCameraType.rear,
-      // resolutionPreset: ResolutionPreset.high,
-      title: 'Object Detection & Tracking',
-      onImage: _detectObjects,
-      overlay: Consumer<ObjectDetectionState>(
-        builder: (_, state, __) {
-          if (state.isEmpty) {
-            return Container();
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (!featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content:
+                    const Text('This feature has not been implemented yet')));
+          } else {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
           }
-
-          Size originalSize = state.size!;
-          Size size = MediaQuery.of(context).size;
-
-          // if image source from gallery
-          // image display size is scaled to 360x360 with retaining aspect ratio
-          if (state.notFromLive) {
-            if (originalSize.aspectRatio > 1) {
-              size = Size(360.0, 360.0 / originalSize.aspectRatio);
-            } else {
-              size = Size(360.0 * originalSize.aspectRatio, 360.0);
-            }
-          }
-
-          return ObjectOverlay(
-            size: size,
-            originalSize: originalSize,
-            rotation: state.rotation,
-            objects: state.data,
-          );
         },
       ),
     );
-  }
-}
-
-class ObjectDetectionState extends ChangeNotifier {
-  InputImage? _image;
-  List<DetectedObject> _data = [];
-  bool _isProcessing = false;
-
-  InputImage? get image => _image;
-  List<DetectedObject> get data => _data;
-
-  String? get type => _image?.type;
-  InputImageRotation? get rotation => _image?.metadata?.rotation;
-  Size? get size => _image?.metadata?.size;
-
-  bool get isNotProcessing => !_isProcessing;
-  bool get isEmpty => _data.isEmpty;
-  bool get isFromLive => type == 'bytes';
-  bool get notFromLive => !isFromLive;
-
-  void startProcessing() {
-    _isProcessing = true;
-    notifyListeners();
-  }
-
-  void stopProcessing() {
-    _isProcessing = false;
-    notifyListeners();
-  }
-
-  set image(InputImage? image) {
-    _image = image;
-
-    if (notFromLive) {
-      _data = [];
-    }
-    notifyListeners();
-  }
-
-  set data(List<DetectedObject> data) {
-    _data = data;
-    notifyListeners();
   }
 }
