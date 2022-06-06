4b9e2e26711b452977b7abdc76305421521aa141
hanuuuuU <han980004@naver.com>
2022-06-07 02:22:48 +0900

    add contactus

--- lib/screen/contact_us.dart
+++ lib/screen/contact_us.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:contactus/contactus.dart';
 import 'package:gdsc_solution/theme/custom_color.dart';
 import 'package:typicons_flutter/typicons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
